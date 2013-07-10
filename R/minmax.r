4cbcb1a33fc00877a296b2f00e3a4fd7000a65fe
unknown <julii@IKBM-B19011444.ans.umb.no>
2013-07-10 09:59:46 +0200

    MinMax function is added - a helper function for heatmap.combine

---
+++ R/minmax.r
@@ -0,0 +1,17 @@
+#' Generates a matrix with a minimum and maximum value for xvect and yvect - helper function for heatmap plot
+#'
+#' @param xvect a numerical vector
+#' @param yvect a numerical vector
+#' @export
+#' 
+MinMax = function(xvect, yvect) {
+  MinMaxMatrix = base::matrix(0,nrow=2,ncol=2)
+  MinMaxMatrix = base::as.data.frame(MinMaxMatrix)
+  names(MinMaxMatrix) = base::c('xvect', 'yvect')
+  row.names(MinMaxMatrix) = base::c('min', 'max')
+  MinMaxMatrix['min', 'xvect'] = base::min(xvect, na.rm=T)
+  MinMaxMatrix['max', 'xvect'] = base::max(xvect, na.rm=T)
+  MinMaxMatrix['min', 'yvect'] = base::min(yvect, na.rm=T)
+  MinMaxMatrix['max', 'yvect'] = base::max(yvect, na.rm=T)
+  # return(MinMaxMatrix)  
+}
