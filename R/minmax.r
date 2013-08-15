414372e31324913069e3e68b3011b71e885b830a
unknown <julii@IKBM-B19011444.ans.umb.no>
2013-08-15 11:15:00 +0200

    return random number

--- R/minmax.r
+++ R/minmax.r
@@ -15,6 +15,7 @@ MinMax = function(xvect, yvect) {
   MinMaxMatrix['max', 'yvect'] = base::max(yvect, na.rm=T)
   
   random_number = stats::runif(1, 1, 1.1)
+  base::return(random_number)
 #   MinMaxMatrix['min', 'xvect'] =  random_number * MinMaxMatrix['min', 'xvect']
 #   MinMaxMatrix['max', 'xvect'] = random_number * MinMaxMatrix['max', 'xvect'] # * runif(1, 1, 1.1)
 #   MinMaxMatrix['min', 'yvect'] = random_number * MinMaxMatrix['min', 'yvect'] # * runif(1, 1, 1.1)
